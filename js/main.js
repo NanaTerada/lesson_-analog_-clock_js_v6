c8c13cc117f36a67e15ad045dba0e4ed8024e3a8
n2 <7worki.nprogress@gmail.com>
2020-05-20 17:45:11 +0900

    #04 短い目盛りを描画しよう

--- js/main.js
+++ js/main.js
@@ -1,11 +1,15 @@
 'use strict' ;
+
 {
     // クラス
     class Clock {
+            constructor() {
+                this.r = 100;
+            }
         
 
             // 時計の盤面を描画するメソッド
-            this.drawFace() {
+            drawFace() {
                 const canvas = document.querySelector('canvas');
                 
                 if (typeof canvas.getContext === 'undefined') {
@@ -23,10 +27,15 @@
 
                     //原点をキャンバスの中心に移動させる
                     ctx.translate(width / 2, height / 2);
-                    ctx.rotate(2 * Math.PI / 360 * angle);
+                    ctx.rotate(2 * Math.PI / 360 * angle); //アングルをラジアンに変換しつつ。。。？
 
+                    // 短い線を描いていく
+                    ctx.beginPath();
+                    ctx.moveTo(0,-this.r); //中心から半径の位置まで　-this.r←マイナス方向に半径分
+                    ctx.lineTo(0,-this.r + 5); //線の長さ
+                    ctx.stroke(); //線をひく
 
-                    ctx.restore();
+                    ctx.restore();//次のループの時に座標空間をもどしたいので
                 } 
             }
 
