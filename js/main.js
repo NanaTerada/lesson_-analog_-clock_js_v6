5b774e48121e37edbb1e80e506839119e05fdaa0
n2 <7worki.nprogress@gmail.com>
2020-05-20 17:36:14 +0900

    #03 盤面を描く準備をしよう

--- js/main.js
+++ js/main.js
@@ -16,6 +16,18 @@
 
                 const width = canvas.width;
                 const height = canvas.height;
+
+                // 360 / 60 = 6 6度ずつ回転しながら描画
+                for (let angle = 0; angle <360; angle += 6) {
+                    ctx.save(); //次のループの時に座標空間をもどしたいので
+
+                    //原点をキャンバスの中心に移動させる
+                    ctx.translate(width / 2, height / 2);
+                    ctx.rotate(2 * Math.PI / 360 * angle);
+
+
+                    ctx.restore();
+                } 
             }
 
             run() {
